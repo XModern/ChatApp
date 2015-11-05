9b1264afd0c1228ce1975016c3ec5c10efb8a8a7
XModern <themadpro9rammer@gmail.com>
2015-11-05 22:44:21 +0200

    Connection

---
+++ src/Connection.java
@@ -0,0 +1,95 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class Connection 
+{
+	private final static int port=7777;
+	//private int ip;
+	private ServerSocket serverSocket;
+	private Socket socket;
+	public void sendMessage(String Text)
+	{
+		try
+		{
+			OutputStream out=socket.getOutputStream();
+			DataOutputStream outC = new DataOutputStream(out);
+		              	
+	        	outC.writeUTF(Text);
+	        	outC.flush();
+		}
+		catch(IOException e)
+		{
+			System.out.println("IO Error!!! (sendMessage)");
+		}
+	}
+	public void receiveMessage()
+	{
+		try
+		{
+			InputStream in= socket.getInputStream();
+			DataInputStream inC = new DataInputStream(in);
+			String line = null;
+			
+				line=inC.readUTF();
+				System.out.println("line: "+line+"; IP: "+socket.getInetAddress()+"; port: "+socket.getLocalPort());
+				sendMessage("Message has been received");
+
+		}
+		catch(IOException e)
+		{
+			System.out.println("IO Error!!! (receiveMessage)");
+		}
+	}
+	public void connectionToMe()
+	{
+		try
+		{
+			System.out.println("Waiting for a client...");
+			serverSocket=new ServerSocket(port);
+			socket= serverSocket.accept();
+			System.out.println("Got a client!");
+			sendMessage("Successfully connected");
+		}
+		catch(IOException e)
+		{
+			System.out.println("IO Error!!! (connectionToMe)");
+		}
+		
+	}
+	public void connectionFromMe(String ipStr)
+	{
+		try
+		{
+			InetAddress ip= InetAddress.getByName(ipStr);
+			socket= new Socket(ip,port);
+			receiveMessage();
+			//System.out.println("Successfully connected");
+		}
+		catch(IOException e)
+		{
+			System.out.println("IO Error!!! (connectionFromMe)");
+		}
+	}
+	public void disconnect()
+	{
+		try 
+		{
+			sendMessage("Disconnect");
+			socket.close();
+		} 
+		catch (IOException e) 
+		{
+			System.out.println("IO Error!!! (disconnect)");
+		}
+	}
+
+}
--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
-public class Test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!!!");
+
+public class Test 
+{
+
+	public static void main(String[] args) throws IOException 
+	{
+		Connection client= new Connection();
+		//client.connectionToMe();//k komu podkluchaiutsa
+		
+		client.connectionFromMe("127.0.0.1");//kto podkluchaetsa
+		client.sendMessage("Hello");//
+		
+		
+		
+		
+		
+		
+		
+		/*ConnectionFail a = new Connection();
+		//a.connectionToMe();
+		
+		a.connectionFromMe("127.0.0.1");
+		BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+		BuffferedReader t = new BufferedReader(new InputStreamReader(new InputStream())));
+		a.sendMessage(keyboard);*/
+		
 	}
 
 }
